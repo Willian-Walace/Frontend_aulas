3becac98602f4ea915444127061d5d8a0a8ec9d8
Willian-Walace <willianwalacemedeirosj@gmail.com>
2025-09-25 21:41:35 -0300

    Somativa
    Somativa

--- tatiana/SOMATIVA/src/views/MaintenanceDetail.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="p-6">
-    <h2 class="text-2xl font-bold mb-4">Detalhes da Manutenção</h2>
-    <p>Aqui serão exibidas informações completas sobre a manutenção selecionada.</p>
-  </div>
-</template>
--- tatiana/SOMATIVA/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/postcss'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), tailwindcss()]
-  
-})
